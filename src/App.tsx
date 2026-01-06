baf7c6c7c138194a23d52a33d17cb49718e5142e
Mike Cann <mike.cann@gmail.com>
2026-01-06 10:28:18 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../convex/_generated/api";
+
+export default function App() {
+  return (
+    <>
+      <header className="sticky top-0 z-10 bg-light dark:bg-dark p-4 border-b-2 border-slate-200 dark:border-slate-800">
+        Convex + React
+      </header>
+      <main className="p-8 flex flex-col gap-16">
+        <h1 className="text-4xl font-bold text-center">Convex + React</h1>
+        <Content />
+      </main>
+    </>
+  );
+}
+
+function Content() {
+  return null;
+}
